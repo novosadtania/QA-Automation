a50c653eeb90571f749fd90f4aeba9acfd61a439
Tetiana <tanya.novosad.99@gmail.com>
2023-06-30 15:53:03 +0300

    Hom

---
+++ src/main/java/homework1/Homework1.java
@@ -0,0 +1,35 @@
+package homework1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Homework1 {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\AdminP\\Desktop\\Selen\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+//        1
+        /*driver.get("https://uhomki.com.ua/koshki/1074/");
+        driver.findElement(By.xpath( "//span[@class = 'userbar__button-text']")).click();
+        Thread.sleep(2000);
+        driver.quit();*/
+//        2
+        /*driver.get("https://uhomki.com.ua/koshki/1074/");
+        driver.findElement(By.xpath("//span[text() = 'Вход']")).click();
+        Thread.sleep(2000);
+        driver.quit();*/
+//        3
+      /*  driver.get("https://uhomki.com.ua/koshki/1074/");
+        driver.findElement(By.xpath("//span[contains(@class, 'userbar__button')]")).click();
+        Thread.sleep(2000);
+        driver.quit();*/
+//        4
+       /* driver.get("https://uhomki.com.ua/koshki/1074/");
+        driver.findElement(By.xpath("//span[contains(text(), 'ход')]")).click();
+        Thread.sleep(2000);
+        driver.quit();*/
+
+
+    }
+
+}
---
+++ src/main/java/homework1/Homework2.java
@@ -0,0 +1,18 @@
+package homework1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Homework2 {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\AdminP\\Desktop\\Selen\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://uhomki.com.ua/ru/koshki/1074/");
+        driver.findElement(By.xpath( "//a[@class = 'footer__link']")).click();
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
+
