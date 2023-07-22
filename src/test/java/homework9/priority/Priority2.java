b65a79d4df1ea587e3ec9c77588ed703bdc678b1
Tetiana <tanya.novosad.99@gmail.com>
2023-07-22 17:51:33 +0300

    .

---
+++ src/test/java/homework9/priority/Priority2.java
@@ -0,0 +1,28 @@
+package homework9.priority;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class Priority2 {
+    @Test(dependsOnMethods = "b")
+    public void a() {assertTrue (true);}
+
+    @Test(dependsOnMethods = "c")
+    public void b() {assertTrue (true);}
+
+    @Test(dependsOnMethods = "d")
+    public void c() {assertTrue (true);}
+
+    @Test(dependsOnMethods = "e")
+    public void d() {assertTrue (true);}
+
+    @Test(dependsOnMethods = "f")
+    public void e() {assertTrue (true);}
+
+    @Test(dependsOnMethods = "g")
+    public void f() {assertTrue (true);}
+
+    @Test
+    public void g() {assertTrue (true);}
+}
